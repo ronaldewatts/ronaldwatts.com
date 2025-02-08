a74831eafb35b7b9d80533fe31c85efe63a29538
ron <ronaldewatts@gmail.com>
2025-02-08 17:49:16 -0600

    Add github pages deployment

---
+++ src/Router.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
+
+import Home from './features/Home';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact={true} path="/" component={Home} />
+                <Route component={() => <Redirect to="/" />} />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
