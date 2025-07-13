11f328760042afe06ed576ede88b93cb69d51e47
ronaldewatts <ronaldewatts@gmail.com>
2025-07-13 06:22:46 -0500

    update the fiji plugin version

--- scripts/update-fiji-plugin.sh
+++ scripts/update-fiji-plugin.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 sourceLoc=$1
-version=$2
-pluginTarget=$3
+pluginTarget=$2
+version=$3
 localPluginSite="uab-fiji-plugins-local"
 
-cd ~/applications/fiji/ || exit
+cd ~/projects/ronaldwatts.com/applications/fiji/ || exit
 ./fiji --update remove-update-site "$localPluginSite"
 ./fiji --update update
 ./fiji --update add-update-site "$localPluginSite" "file:$pluginTarget" "file:" "$pluginTarget"
